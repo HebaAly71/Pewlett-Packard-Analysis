c35dc7a7e57fe40be16b9aec7de977c0051432d8
HebaAly71 <47098642+HebaAly71@users.noreply.github.com>
2020-01-10 16:16:55 -0800

    final version

--- schema.sql
+++ schema.sql
@@ -31,15 +31,6 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
-
-CREATE TABLE titles (
-  emp_no INT NOT NULL,
-  title varchar NOT NULL,
-  from_date DATE NOT NULL,
-  to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
-);
 CREATE TABLE dept_emp (
 emp_no INT NOT NULL,
     dept_no VARCHAR(4) NOT NULL,
@@ -48,4 +39,13 @@ emp_no INT NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 	PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE titles (
+  emp_no INT NOT NULL,
+  title varchar NOT NULL,
+  from_date DATE NOT NULL,
+  to_date DATE NOT NULL,
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+  FOREIGN KEY (from_date) REFERENCES dept_emp (from_date),
+  PRIMARY KEY (emp_no, from_date)
 );
